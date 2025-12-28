0dfcc572f17f80cec27c2c166f7dfa4657403f1c
usmcvito <jegenovese@hotmail.com>
2025-12-28 12:05:22 -0500

    Start of Learning Python

---
+++ Hello.py
@@ -0,0 +1,44 @@
+# Autor: Vito
+# New Info
+
+# DATA TYPES:
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+
+# String
+first_name = "Mike"
+print(first_name)
+
+# Number
+age = 28
+print(age)
+
+# List
+names = ['John', 'Mary', 'Bob']
+print(names[0])
+print(names)
+
+# Tuple (speeds up processing
+tnames = ('John', 'Mary', 'Bob')
+print(tnames[0])
+print(tnames)
+
+# Dictionary (key-value pairs)
+fav_pizza = {
+    "John": "Pepperoni",
+    "Mary": "Mushrooms",
+    "Bob": "Cheese"
+}
+print(fav_pizza["John"])
+print(fav_pizza["Mary"])
+print(fav_pizza["Bob"])
+
+# Boolean (True or False)
+
+name = True
+print(name)
+print(type(name))
